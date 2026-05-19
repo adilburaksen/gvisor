69203a286912f88453a661ce4e282651f9299760
Lucas Manning <lucasmanning@google.com>
2026-05-19 15:37:37 -0700

    Automatically enable tpuproxy based on devices in the spec.
    PiperOrigin-RevId: 918063946

--- runsc/specutils/specutils.go
+++ runsc/specutils/specutils.go
@@ -658,12 +658,9 @@ func IsDebugCommand(conf *config.Config, command string) bool {
 	return !rv
 }
 
-// TPUProxyIsEnabled checks if tpuproxy is enabled in the config or annotations.
-func TPUProxyIsEnabled(spec *specs.Spec, conf *config.Config) bool {
-	if conf.TPUProxy {
-		return true
-	}
-	return AnnotationToBool(spec, AnnotationTPU)
+// TPUProxyEnabled checks if tpuproxy is enabled in the config or annotations.
+func TPUProxyEnabled(spec *specs.Spec, conf *config.Config) bool {
+	return conf.TPUProxy || AnnotationToBool(spec, AnnotationTPU) || TPUFunctionalityRequested(spec, conf)
 }
 
 // VFIOFunctionalityRequested returns true if the container should have access
@@ -682,9 +679,6 @@ func AcceleratorFunctionalityRequested(dev *specs.LinuxDevice) bool {
 // TPUFunctionalityRequested returns true if the container should have access
 // to TPU functionality.
 func TPUFunctionalityRequested(spec *specs.Spec, conf *config.Config) bool {
-	if !TPUProxyIsEnabled(spec, conf) {
-		return false
-	}
 	if spec.Linux != nil {
 		for _, dev := range spec.Linux.Devices {
 			if AcceleratorFunctionalityRequested(&dev) || VFIOFunctionalityRequested(&dev) {
